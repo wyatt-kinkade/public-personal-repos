d17cd9212cc8cb0bc089f8c4b6ac553cf4c3ce56
xifgay <janny@localhost.localdomain>
2021-08-24 10:07:59 -0400

    removing file

--- oui_lookup.sh
+++
@@ -1,8 +0,0 @@
-read -p "MAC Address or OUI to search: " MAC_VAL
-
-#Ripping out common delimiters and then only using the first 6 characters of the string
-REFINED_MAC=`echo "$MAC_VAL" | tr -d '.' | tr -d '-' | tr -d ':' | head -c 6`
-
-#Dodging awk to speed things up
-curl -sS "http://standards-oui.ieee.org/oui/oui.txt" | grep -i "$REFINED_MAC" | cut -d')' -f2 | tr -d '\t'
-
